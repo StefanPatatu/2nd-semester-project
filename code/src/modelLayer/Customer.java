89f66d2a73ef6e9798d0037b1105c0cdc9ba8d21
TheDarkSun <mwmwmw25@gmail.com>
2015-05-14 11:11:35 +0200

    Class Design

---
+++ code/src/modelLayer/Customer.java
@@ -0,0 +1,77 @@
+package modelLayer;
+
+/**
+ * Customer
+ * 
+ * @author Monika
+ * @version 1.0
+ */
+
+public class Customer {
+	private String name;
+	private String city;
+	private String street;
+	private String phoneNr;
+	private String email;
+	private int discount;
+
+	
+	//constructor	
+	public Customer (
+			String name,
+			String city,
+			String street,
+			String phoneNr,
+			String email)	{
+		this.setName(name);
+		this.setCity(city);
+		this.setStreet(street);
+		this.setPhoneNr(phoneNr);
+		this.setEmail(email);
+		discount = 0;
+	}
+    
+	//name
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+    //city
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	//street
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	//phoneNr
+	public String getPhoneNr() {
+		return phoneNr;
+	}
+	public void setPhoneNr(String phoneNr) {
+		this.phoneNr = phoneNr;
+	}
+	//email
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	//discount
+	public int getDiscount() {
+		return discount;
+	}
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}	
+
+}
