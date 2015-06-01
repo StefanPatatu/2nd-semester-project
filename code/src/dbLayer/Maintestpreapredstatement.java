a52264664caf908e112a3c1a5a2436f3e6abce74
futz <stefan.patatu@gmail.com>
2015-06-01 12:41:54 +0200

    test class in db

--- code/src/dbLayer/Maintestpreapredstatement.java
+++ code/src/dbLayer/Maintestpreapredstatement.java
@@ -1,14 +1,17 @@
 package dbLayer;
 
+import java.util.ArrayList;
+
 import authLayer.DbConfig;
 import modelLayer.Item;
+import controlLayer.CtrItem;
 
 public class Maintestpreapredstatement {
 	
-	private static DbItem dbItem;
+	private static CtrItem ctrItem;
 
 	public static void main(String[] args) {
-		dbItem = new DbItem();
+		ctrItem = new CtrItem();
 		try {
 			DbConfig dbconfing = new DbConfig();
 		} catch (Exception e1) {
@@ -17,11 +20,42 @@ public class Maintestpreapredstatement {
 		}
 		
 		try {
-			insertItemTest(createItem());
+			Item item = ctrItem.findItemByBarcode("barcode");
+			
+			ArrayList<Item> items = ctrItem.searchItemsByStockRange(1, 1);
+			
+			System.out.println(items.get(0).getName());
+			
+			System.out.println(item.getBarcode());
+			System.out.println(item.getCategory());
+			System.out.println(item.getId_item());
+			System.out.println(item.getItemType());
+			System.out.println(item.getName());
+			System.out.println(item.getPrice());
+			System.out.println(item.getStock());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		/*
+		try {
+			Item item = createItem();
+			
+			System.out.println(insertItemTest(item));
+			
+			item = ctrItem.findItemByBarcode("barcode");
+			System.out.println(item.getBarcode());
+			System.out.println(item.getCategory());
+			System.out.println(item.getId_item());
+			System.out.println(item.getItemType());
+			System.out.println(item.getName());
+			System.out.println(item.getPrice());
+			System.out.println(item.getStock());
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-
+	*/
 	}
 	
 	private static Item createItem() {
@@ -29,9 +63,8 @@ public class Maintestpreapredstatement {
 		return item;
 	}
 	
-	private static int insertItemTest(Item item) throws Exception {
-		dbItem.insertItem(item);
-		return 0;
+	private static int insertItemTest() throws Exception {
+		return ctrItem.insertItem("12", "name", 12, 1, "type", "cat");
 	}
 
 }
